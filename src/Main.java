5902f45e0ce1187979d9fb595b337bfc6155b604
MaximGol11 <maxim.golubtsov@yahoo.com>
2021-09-07 17:30:47 +0300

    new main

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,46 @@
-import java.util.Arrays;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        int summ = 0;
-        int summ2 = 0;
-        double minNumber = 0;
-        int maxNumber = 0;
+
+        //initial variables and strings
+        String str1 = "Перестановочный алгоритм быстрого действия";
+        String str2 = "Перевыборы выбранного президента";
+        String str3 = "Посмотрите как Рите нравится ритм";
+        String[][] array = {{"Привет", "всем", "кто"}, {"изучает", "язык", "программирования"}, {"java"}};
         int counter = 0;
-        int[] nums = new int[] {9, 2, 6, 4, 5, 12, 7, 8, 6};
-        int[][] nums2 = new int[][] {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
-        double[][] nums3 = new double[][] {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int extraCounter = 0;
 
-
-        // tasks 1-4
-
-        for (int i = 0; i < nums.length; i++) {
-            summ += nums[i];
+        //task #1
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == 'о') {
+                System.out.print(str1.charAt(i));
+            }
         }
-        System.out.println("sum = " + summ);
 
-        Arrays.sort(nums);
-        System.out.println("min = " + nums[0]);
-        System.out.println("max = " + (nums.length-1));
-        System.out.println("average = " + (summ / nums.length));
-
-        //tasks 5-7
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = 0; j < nums2[i].length; j++) {
-                // sum all array element
-                summ2 += nums2[i][j];
-                // count all array element
+        // task #2
+        for (int i = 0; i < str2.length(); i++) {
+            if (str2.charAt(i) == 'е') {
                 counter += 1;
-                // maximum array element
-                if (maxNumber <= nums2[i][j]) {
-                    maxNumber = nums2[i][j];
-                } else {
-                    continue;
-                }
             }
         }
-        // min array element
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = 0; j < nums3[i].length; j++) {
-                // maximum array element
-                if (minNumber >= nums3[i][j]) {
-                    minNumber = nums3[i][j];
-                } else {
-                    continue;
-                }
-            }
+        System.out.println(counter);
+
+        // task #3
+        String str3_lower = str3.toLowerCase();
+        int index = str3_lower.indexOf("рит");
+
+        while (index != -1) {
+            System.out.println(index);
+            index = str3_lower.indexOf("рит", index + 1);
         }
-        System.out.println("sum = " + summ2);
-        System.out.println("max = " + maxNumber);
-        System.out.println("min = " + minNumber);
-        System.out.println("count = " + counter);
+
+        //extra task !!!
+        for ( int i = 0; )
 
 
-    }
 
 
-    }
+
+}
+}
